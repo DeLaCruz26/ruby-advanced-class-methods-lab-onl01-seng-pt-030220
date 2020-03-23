ef4774d270bf570837ed16ff6500ed1670e81e5e
Dametrik Fick <dametrik.fick@gmail.com>
2020-03-23 21:15:16 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -53,6 +53,7 @@ class Song
     split_filemane = filename.chomp(."mp3").split(" - ")
     song.name = split_filemane[1]
     song.artist_name = split_filemane[0]
+    song.save
     song
   end
 end
